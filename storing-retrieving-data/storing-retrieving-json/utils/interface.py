ef592f9fe14a0794b0299941efa4800b4d4fa655
pranjalgupta610 <95283368+pranjalgupta610@users.noreply.github.com>
2022-06-08 00:00:31 +0530

    Create interface.py
    manages the data in .json file

---
+++ storing-retrieving-data/storing-retrieving-json/utils/interface.py
@@ -0,0 +1,50 @@
+"""Concerned with storing and retrieving books from .json file. Format:
+[
+    {
+        'name': 'BookName'
+        'author': 'AuthorName'
+        'read': True
+    }
+]"""
+import json
+
+books_file = 'books.json'
+
+
+def create_book_file():
+    """Makes sure json file exists"""
+    with open(books_file,'w') as file:
+        json.dump([], file)
+        pass
+
+
+def add_book(name, author):
+    books = get_all_books()
+    books.append({'name': name,'author': author,'read': False})
+    _save_all_books(books)
+
+
+def get_all_books():
+    with open(books_file, 'r') as file:
+        return json.load(file)
+
+
+def _save_all_books(books):
+    with open('books.json','w') as file:
+        json.dump(books,file)
+
+
+def mark_book_as_read(name):
+    books = get_all_books()
+    for book in books:
+        if book['name'] == name:
+            book['read'] = True
+
+    _save_all_books(books)
+
+
+def delete_book(name):
+    books = get_all_books()
+    books = [book for book in books if book['name'] != name]
+
+    _save_all_books(books)
